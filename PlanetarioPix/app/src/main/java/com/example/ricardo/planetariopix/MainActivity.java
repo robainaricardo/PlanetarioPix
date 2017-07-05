a02e53cd8d0e627c3aa426c002859abf8a2c0147
Ricardo P. Robaina <ricardorobaina@outlook.com>
2017-07-05 20:34:12 -0300

    BOTAO GALERIA E REMOÇÃO DE FUNDO FUNCIONANDO
    Botao galeria com funcionalidade funcionando perfeitamente.
    
    Primeira versão da função de remoção de fundo funcionando

--- PlanetarioPix/app/src/main/java/com/example/ricardo/planetariopix/MainActivity.java
+++ PlanetarioPix/app/src/main/java/com/example/ricardo/planetariopix/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ricardo.planetariopix;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,12 +13,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1; //variavel da intent da foto
+    static final int GALERIA_IMAGEM = 2; //variavel da intent da Galeria
 
     private ImageView imViewFoto;
     private Button btTirarFoto;
+    private Button btGaleria;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +33,23 @@ public class MainActivity extends AppCompatActivity {
         //--começo codigo
         imViewFoto = (ImageView) findViewById(R.id.imViewFoto);
         btTirarFoto = (Button) findViewById(R.id.btTirarFoto);
+        btGaleria = (Button) findViewById(R.id.btGaleria);
 
-        btTirarFoto.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        btTirarFoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
         });
+
+        btGaleria.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //chamar galeria
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, GALERIA_IMAGEM);
+
+            }
+        });
+
     }
 
     //método que chama a camera
@@ -47,6 +67,43 @@ public class MainActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             //usar aqui a função de processsamento de imagem
             imViewFoto.setImageBitmap(imageBitmap);
+        } else if (resultCode == RESULT_OK && requestCode == GALERIA_IMAGEM) {
+            Uri selectedImage = data.getData();
+            String[] filePath = {MediaStore.Images.Media.DATA};
+            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+            //usar aqui a função de processsamento de imagem
+
+            //imViewFoto.setImageBitmap(thumbnail);
+
+            thumbnail = removeFundoVerde(thumbnail);
+            imViewFoto.setImageBitmap(thumbnail);
+
         }
     }
+
+
+
+    public static Bitmap removeFundoVerde(Bitmap srcBitmap){
+        Bitmap result = srcBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        int nWidth = result.getWidth();
+        int nHeight = result.getHeight();
+
+        for (int y = 0; y < nHeight; ++y) {
+            for (int x = 0; x < nWidth; ++x) {
+                int nPixelColor = result.getPixel(x, y);
+                float hsv[] = new float[3];
+                Color.colorToHSV(nPixelColor, hsv);
+                if(hsv[0] >= 60 && hsv[0] <= 130 && hsv[1] >= 0.4 && hsv[2] >= 0.3)
+                    result.setPixel(x, y, Color.TRANSPARENT);
+            }
+        }
+        return result;
+        }
+
+
 }
