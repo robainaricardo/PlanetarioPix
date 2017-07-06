c3781a13c83db583b47b087b4034684907421843
Ricardo P. Robaina <ricardorobaina@outlook.com>
2017-07-05 22:52:04 -0300

    Merge das imagens funcionando PROBLEMA DE TRANSPARENCIA

--- PlanetarioPix/app/src/main/java/com/example/ricardo/planetariopix/MainActivity.java
+++ PlanetarioPix/app/src/main/java/com/example/ricardo/planetariopix/MainActivity.java
@@ -4,15 +4,21 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -81,7 +87,10 @@ public class MainActivity extends AppCompatActivity {
             //imViewFoto.setImageBitmap(thumbnail);
 
             thumbnail = removeFundoVerde(thumbnail);
-            imViewFoto.setImageBitmap(thumbnail);
+            Bitmap fundo = new BitmapFactory().decodeResource(getResources(), R.drawable.lua_jupter);
+
+            Bitmap pronto = overlay(fundo, thumbnail);
+            imViewFoto.setImageBitmap(pronto);
 
         }
     }
@@ -97,13 +106,54 @@ public class MainActivity extends AppCompatActivity {
             for (int x = 0; x < nWidth; ++x) {
                 int nPixelColor = result.getPixel(x, y);
                 float hsv[] = new float[3];
+
                 Color.colorToHSV(nPixelColor, hsv);
-                if(hsv[0] >= 60 && hsv[0] <= 130 && hsv[1] >= 0.4 && hsv[2] >= 0.3)
+                if(hsv[0] >= 60 && hsv[0] <= 130 && hsv[1] >= 0.4 && hsv[2] >= 0.3){
                     result.setPixel(x, y, Color.TRANSPARENT);
+                    //result.eraseColor(Color.TRANSPARENT);
+                }
+
+                //=====================================
+                //FUNÇÂO QUE ARRUMA AS BORDAS DA IMAGEM
+                //=====================================
+                //if(hsv[0] >= 60 && hsv[0] <= 130 && hsv[1] >= 0.15 && hsv[2] > 0.15) {
+                  //  if ((r * b) != 0 && (g * g) / (r * b) >= 1.5) {
+                   //     result.setIntColor(x, y, 255, (int) (r * 1.4), (int) g, (int) (b * 1.4));
+                    //} else {
+                     //   image.setIntColor(x, y, 255, (int) (r * 1.2), g, (int) (b * 1.2));
+                    //}
             }
         }
+
+
+
+
+
+
+
         return result;
+    }
+
+
+
+    public static Bitmap overlay(Bitmap bmp1, Bitmap bmp2) {
+        try
+        {
+            Bitmap bmOverlay = Bitmap.createBitmap(bmp1.getWidth(), bmp1.getHeight(),  bmp1.getConfig());
+            Canvas canvas = new Canvas(bmOverlay);
+            canvas.drawBitmap(bmp1, new Matrix(), null);
+            canvas.drawBitmap(bmp2, 0, 0, null);
+            return bmOverlay;
+        } catch (Exception e)
+        {
+            // TODO: handle exception
+            e.printStackTrace();
+            return null;
         }
+    }
+
+
+
 
 
 }
