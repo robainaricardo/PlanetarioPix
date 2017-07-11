b4618ed983d3682a22d0d7f15a4a8ba3d5cee812
Ricardo P. Robaina <ricardorobaina@outlook.com>
2017-07-11 10:47:28 -0300

    Problema da Transparência RESOLVIDO
    Problema da Transparência do bitmap no overlay resolvido

--- PlanetarioPix/app/src/main/java/com/example/ricardo/planetariopix/GaleriaActivity.java
+++ PlanetarioPix/app/src/main/java/com/example/ricardo/planetariopix/GaleriaActivity.java
@@ -1,13 +1,56 @@
 package com.example.ricardo.planetariopix;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class GaleriaActivity extends AppCompatActivity {
+
+    private ImageButton imb1;
+    private ImageButton imb2;
+    private ImageButton imb3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeria);
+
+        ImageButton imb1 = (ImageButton) findViewById(R.id.imb1);
+        ImageButton imb2 = (ImageButton) findViewById(R.id.imb2);
+        ImageButton imb3 = (ImageButton) findViewById(R.id.imb3);
+
+        imb1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                retorno(1);
+            }
+        });
+
+        imb2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //chamar galeria
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("IMAGEM",2);
+                setResult(RESULT_OK,returnIntent);
+            }
+        });
+
+        imb3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //chamar galeria
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("IMAGEM",3);
+                setResult(RESULT_OK,returnIntent);
+            }
+        });
+
+    }
+
+    void retorno(int imagem){
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("IMAGEM",imagem);
+        setResult(RESULT_OK,returnIntent);
     }
 }
--- PlanetarioPix/app/src/main/java/com/example/ricardo/planetariopix/MainActivity.java
+++ PlanetarioPix/app/src/main/java/com/example/ricardo/planetariopix/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1; //variavel da intent da foto
     static final int GALERIA_IMAGEM = 2; //variavel da intent da Galeria
+    static final int ACTIVITY_2 = 3; //Retorno da segunda activity
 
     private ImageView imViewFoto;
     private Button btTirarFoto;
@@ -86,13 +89,30 @@ public class MainActivity extends AppCompatActivity {
 
             //imViewFoto.setImageBitmap(thumbnail);
 
-            //thumbnail = removeFundoVerde(thumbnail);
-            //Bitmap fundo = new BitmapFactory().decodeResource(getResources(), R.drawable.img1);
-            Intent it = new Intent(this, GaleriaActivity.class);
-            startActivity(it);
-            //Bitmap pronto = overlay(fundo, thumbnail);
-            //imViewFoto.setImageBitmap(pronto);
+            thumbnail = colorToAlpha(thumbnail);
+            //imViewFoto.setImageBitmap(thumbnail);
+            Bitmap fundo = new BitmapFactory().decodeResource(getResources(), R.drawable.img2);
 
+            //Intent it = new Intent(this, GaleriaActivity.class);
+            //startActivityForResult(it, ACTIVITY_2);
+
+            Bitmap pronto = overlay(fundo, thumbnail);
+            imViewFoto.setImageBitmap(pronto);
+
+        } else if(resultCode == RESULT_OK && requestCode == ACTIVITY_2){
+            String imagem = data.getStringExtra("IMAGEM");
+            if(imagem == "1"){
+                Bitmap fundo = new BitmapFactory().decodeResource(getResources(), R.drawable.img1);
+                imViewFoto.setImageBitmap(fundo);
+            }
+            if(imagem == "2"){
+                Bitmap fundo = new BitmapFactory().decodeResource(getResources(), R.drawable.img2);
+                imViewFoto.setImageBitmap(fundo);
+            }
+            if(imagem == "3"){
+                Bitmap fundo = new BitmapFactory().decodeResource(getResources(), R.drawable.img3);
+                imViewFoto.setImageBitmap(fundo);
+            }
         }
     }
 
@@ -110,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Color.colorToHSV(nPixelColor, hsv);
                 if(hsv[0] >= 60 && hsv[0] <= 130 && hsv[1] >= 0.4 && hsv[2] >= 0.3){
-                    result.setPixel(x, y, Color.TRANSPARENT);
+                    result.setPixel(x, y, 0);
                     //result.eraseColor(Color.TRANSPARENT);
                 }
 
@@ -126,35 +146,59 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-
-
-
-
-
-
+        Bitmap paramBitmap = Bitmap.createBitmap(result.getWidth(), result.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas localObject = new Canvas(paramBitmap);
+        localObject.drawARGB(0, 0, 0, 0);
+        Paint localPaint = new Paint();
+        localPaint.setAlpha(255);
+        localObject.drawBitmap(result, 0.0F, 0.0F, localPaint);
+        //result.eraseColor(Color.TRANSPARENT);
         return result;
+
+        //return imageWithBG;
     }
 
 
 
-    public static Bitmap overlay(Bitmap bmp1, Bitmap bmp2) {
-        try {
-            Bitmap bmOverlay = Bitmap.createBitmap(bmp1.getWidth(), bmp1.getHeight(), bmp1.getConfig());
-            Canvas canvas = new Canvas(bmOverlay);
-            canvas.drawBitmap(bmp1, new Matrix(), null);
-            canvas.drawBitmap(bmp2, 0, 0, null);
-            return bmOverlay;
-        } catch (Exception e) {
-            // TODO: handle exception
-            e.printStackTrace();
-            return null;
+    public static Bitmap overlay(Bitmap paramBitmap1, Bitmap paramBitmap2)
+    {
+        Bitmap localBitmap = Bitmap.createBitmap(paramBitmap1.getWidth(), paramBitmap1.getHeight(), paramBitmap1.getConfig());
+        Canvas localCanvas = new Canvas(localBitmap);
+        localCanvas.drawBitmap(paramBitmap1, 0.0F, 0.0F, null);
+        localCanvas.drawBitmap(paramBitmap2, 10.0F, 10.0F, null);
+        return localBitmap;
+    }
+
+    public static Bitmap colorToAlpha(Bitmap paramBitmap)
+    {
+        Bitmap localBitmap = paramBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        int k = localBitmap.getWidth();
+        int m = localBitmap.getHeight();
+        int i = 0;
+        while (i < m)
+        {
+            int j = 0;
+            while (j < k)
+            {
+                int n = paramBitmap.getPixel(j, i);
+                float localObject[] = new float[3];
+                Color.colorToHSV(n, (float[])localObject);
+                if ((localObject[0] >= 60.0F) && (localObject[0] <= 130.0F) && (localObject[1] >= 0.15D) && (localObject[2] >= 0.15D)) {
+                    localBitmap.setPixel(j, i, 0);
+                }
+                j += 1;
+            }
+            i += 1;
         }
+        paramBitmap = Bitmap.createBitmap(localBitmap.getWidth(), localBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Object localObject = new Canvas(paramBitmap);
+        ((Canvas)localObject).drawARGB(0, 0, 0, 0);
+        Paint localPaint = new Paint();
+        localPaint.setAlpha(255);
+        ((Canvas)localObject).drawBitmap(localBitmap, 0.0F, 0.0F, localPaint);
+        return paramBitmap;
     }
 
-    public static void chamaTela(){
-
-
-    }
 
 
 
